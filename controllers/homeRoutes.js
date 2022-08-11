687d18fd7f2806264fbf14b937a5f72f857f30e3
Elaine Danahy <edanahy22@gmail.com>
2022-08-10 19:12:25 -0500

    homepage edit

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -18,7 +18,8 @@ router.get('/', async (req, res) => {
 
     // Pass serialized data and session flag into template
     res.render('homepage', { 
-      posts
+      posts,
+      logged_in: req.session.logged_in
     });
   } catch (err) {
     res.status(500).json(err);
